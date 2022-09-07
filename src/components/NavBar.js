0f851148eeef6ac444cea113bf16d22099326052
sudaisali <sudaisali420@gmail.com>
2022-09-07 06:32:46 -0700

    Nav Bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 export default function NavBar(props) {
   return (
     <div>
-       <nav className="navbar navbar-expand-lg bg-light">
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
@@ -16,12 +16,12 @@ export default function NavBar(props) {
             <a className="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/">Link</a>
+            <a className="nav-link" href="/">About</a>
           </li>
         </ul>
         <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <button className="btn btn-primary" type="submit">Search</button>
         </form>
       </div>
     </div>
--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types'
 
 
 export default function Textarea(props) {
-   function handleupclick (){
+   function Uppercase (){
     let newtext = text.toUpperCase();
     setText(newtext);
+ }
+ function Lowercase(){
+  let  newtext = text.toLowerCase();
+  setText(newtext);
  }
  function handleonchange (event){
      setText(event.target.value)
@@ -14,16 +18,32 @@ export default function Textarea(props) {
      
     const [text , setText] = useState("Enter Text here")
 
+   let word = text.split(" ").length;
+   let newword =word;
+   if(text.length<=0){
+    word = 0;
+   }
+ 
    
   return (
+    <>
     <div className='container'>
       <div className="mb-3" >
       <h1>{props.title}</h1>
       <textarea className="form-control" value = {text} onChange={handleonchange} id="my-box" rows="8"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleupclick}>Convert to Upper cases</button>
+      <button className="btn btn-primary mx-1" onClick={Uppercase}>Convert to Upper cases</button>
+      <button className="btn btn-primary mx-1"onClick={Lowercase}>Convert to lower case</button>
      
     </div>
+    <div className='container my-3'>
+      
+      <h3>Your Text Summary</h3>
+      <p>{word} words and {text.length} character</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
 Textarea.propTypes={title: PropTypes.string}
